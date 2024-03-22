f50bc661a2a4d06f3239c4ae0e2dea4183257016
swg4020 <swg4020@naver.com>
2024-03-22 16:11:25 +0900

    search

--- src/pages/search/Search.js
+++ src/pages/search/Search.js
@@ -7,9 +7,17 @@ import { Link } from "react-router-dom";
 import { FaSearch } from "react-icons/fa";
 import { Loading } from "../../components/Loading";
 import { PageTitle } from "../../components/PageTitle";
+import { spacing } from "../../components/GlobalStyled";
 
 const Container = styled.section`
   padding: 150px;
+  @media screen and (max-width: 640px) {
+    padding: ${spacing.padding_640} 0 50px ${spacing.padding_640};
+  }
+
+  @media screen and (max-width: 450px) {
+    padding: ${spacing.padding_450} 0 50px ${spacing.padding_450};
+  }
 `;
 
 const SForm = styled.form`
@@ -20,6 +28,13 @@ const SForm = styled.form`
     padding-left: 5px;
     padding-bottom: 10px;
     width: 100%;
+  }
+  @media screen and (max-width: 640px) {
+    padding: ${spacing.padding_640} 0 50px ${spacing.padding_640};
+  }
+
+  @media screen and (max-width: 450px) {
+    padding: ${spacing.padding_450} 0 50px ${spacing.padding_450};
   }
 `;
 
@@ -32,6 +47,19 @@ const ConWrap = styled.div`
   /* display: flex;
   justify-content: space-between;
   flex-wrap: wrap; */
+  @media screen and (max-width: 640px) {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    row-gap: 30px;
+    column-gap: 10px;
+  }
+
+  @media screen and (max-width: 450px) {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    row-gap: 30px;
+    column-gap: 10px;
+  }
 `;
 const Con = styled.div`
   h3 {
@@ -48,8 +76,15 @@ const Bg = styled.div`
 
 const Text = styled.p`
   width: 100%;
-  font-size: 19px;
-  padding: 50px;
+  font-size: 29px;
+  padding: 50px 0 50px 20px;
+`;
+
+const ErrorMessage = styled.p`
+  font-size: 20px;
+  color: gray;
+  font-weight: 600;
+  padding: 10px;
 `;
 
 export const Search = () => {
@@ -80,6 +115,9 @@ export const Search = () => {
     }
   };
 
+  const message = errors?.search?.message;
+  console.log(message);
+
   // const { search } = getValues();
   // =>검색한 값을 가져와서 사용하는 경우 비구조할당으로 줄수있다,
   // console.log(search);
@@ -90,15 +128,14 @@ export const Search = () => {
       <SForm onSubmit={handleSubmit(onSubmit)}>
         <input
           {...register("search", {
-            required: "내용을 입력해주세요",
+            required: "내용을 입력해주세요..",
           })}
           type="text"
           placeholder="찾으시는 영화가 있나요?"
         />
-        <FaSearch />
       </SForm>
 
-      {errors ? errors?.search?.message : ""}
+      {errors ? <ErrorMessage>{message}</ErrorMessage> : ""}
       {term ? <Text>"{keyword}"의 검색 결과</Text> : ""}
 
       {term && (
@@ -109,7 +146,9 @@ export const Search = () => {
             <>
               {term.map((data) => (
                 <Con key={data.id}>
-                  <Link to={`detail/${data.id}`}>
+                  <Link to={`/detail/${data.id}`}>
+                    {/* =>/가 안붙으면 이전에 있던 기록에 이어서 들어간다 
+                    =>/가 붙으면 홈뒤쪽에 그대로 붙여달라고 할수있다 */}
                     <Bg>
                       {data.poster_path ? (
                         <img
